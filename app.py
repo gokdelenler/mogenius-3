0a4930340c58132b3f1f806d34bb262bb6a82e2f
gokdelenler <106874399+gokdelenler@users.noreply.github.com>
2023-02-07 14:11:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://gokdelenler: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@github.com/gokdelenler/ekle2 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://gokdelenler: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@github.com/gokdelenler/grup2 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
